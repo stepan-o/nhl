d9dda3bd8c548ceb1bd76578ae7d2d1251ea74ea
stepan <stepanoskin007@gmail.com>
2019-07-22 23:33:13 -0400

    Initial commit

--- src/sql.py
+++ src/sql.py
@@ -49,36 +49,30 @@ def create_nhl_draft(table_name, data_path):
 
 def test_nhl_draft(table_name):
     query = (
-        """
-        drop table if exists '{0}'; 
-        """.format(table_name),
-        """
-        create table '{0}'
-        (
-            csplayerid integer,
-            name       varchar(35),
-            nat        char(3),
-            b_date     date,
-            b_place    varchar(50),
-            b_city     varchar(30),
-            b_state    char(2),
-            b_country  varchar(25),
-            ateam      varchar(45),
-            league     varchar(35),
-            year       date,
-            dteam      integer,
-            overall    integer,
-            round      integer,
-            in_round   integer,
-            height     numeric,
-            weight     numeric,
-            pos        varchar(2),
-            shoots     char
-        );
-        """.format(table_name),
-        """
-        alter table '{0}'
-            owner to nhl;
-        """.format(table_name)
+        "drop table if exists '{0}'".format(table_name),
+        "create table '{0}'\
+        (\
+            csplayerid integer,\
+            name       varchar(35),\
+            nat        char(3),\
+            b_date     date,\
+            b_place    varchar(50),\
+            b_city     varchar(30),\
+            b_state    char(2),\
+            b_country  varchar(25),\
+            ateam      varchar(45),\
+            league     varchar(35),\
+            year       date,\
+            dteam      integer,\
+            overall    integer,\
+            round      integer,\
+            in_round   integer,\
+            height     numeric,\
+            weight     numeric,\
+            pos        varchar(2),\
+            shoots     char\
+        );".format(table_name),
+        "alter table '{0}'\
+            owner to nhl;".format(table_name)
     )
     return query
